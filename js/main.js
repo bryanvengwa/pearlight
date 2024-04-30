44feb4bb993e063781a17ed064d126a1151c94d0
bryanvengwa <bmvengwa@gmail.com>
2024-04-30 13:46:44 +0200

    removed scroll error

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-const container = document.querySelector('.categories-container')
-const footer = document.querySelector('.footer-section')
+// const container = document.querySelector('.categories-container')
+// const footer = document.querySelector('.footer-section')
 
 
-window.addEventListener('scroll', function(){
+// window.addEventListener('scroll', function(){
 
-    if (window.scrollY >= container.scrollHeight - 100 ){
-        if(container.classList.contains('fixed')){
-            return
-        }else
-        container.classList.add('fixed')
-    }else if (window.scrollY <= container.scrollHeight + 9000 ){
+//     if (window.scrollY >= container.scrollHeight - 100 ){
+//         if(container.classList.contains('fixed')){
+//             return
+//         }else
+//         container.classList.add('fixed')
+//     }else if (window.scrollY <= container.scrollHeight + 9000 ){
         
-        container.classList.remove('fixed')
-    }
+//         container.classList.remove('fixed')
+//     }
 
 
-})
+// })
